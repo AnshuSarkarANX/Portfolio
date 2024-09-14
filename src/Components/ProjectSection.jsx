1fddf22c5fac5472b15cbdaa6785c26dc5f3f2e1
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-09-14 19:11:45 +0530

    error fixed

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Projects from './projects'
+import Projects from './Projects.jsx'
 
 const ProjectSection = () => {
   return (
     <div className='projectSection h-screen'>
     <h1 className='text-7xl underline'>Projects</h1>
+    <Projects image={''} title = {''} description ={''} techstack={''} link={''}/>
+    <Projects image={''} title = {''} description ={''} techstack={''} link={''}/>
     <Projects image={''} title = {''} description ={''} techstack={''} link={''}/>
     </div>
   )
