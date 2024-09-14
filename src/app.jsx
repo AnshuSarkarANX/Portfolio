064b6ba6ba8b12832a94c2b48edf4baa273a13c9
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-09-15 03:25:31 +0530

    name updated

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ export function App() {
     <div className="app relative">
       <div className="mainSection h-screen">
         <div className="Namebar">
-          <p className="Name text-4xl font-bold drop-shadow-sm">ANSHU SARKAR</p>
+          <p className="Name text-4xl font-bold drop-shadow-sm text-center">ANSHU SARKAR</p>
         </div>
         <div className="rolebox">
           <p className="role text-9xl font-bold drop-shadow-lg">
