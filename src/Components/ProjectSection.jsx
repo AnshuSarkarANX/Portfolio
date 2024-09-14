94f6e7e48170710127400d1ebf501b5bd9af6e5a
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-09-15 03:37:07 +0530

    project details added

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -3,13 +3,21 @@ import Projects from './Projects.jsx'
 
 const ProjectSection = () => {
   return (
-    <div className='projectSection h-screen'>
-    <h1 className='text-7xl underline'>Projects</h1>
-    <Projects image={''} title = {''} description ={''} techstack={''} link={''}/>
-    <Projects image={''} title = {''} description ={''} techstack={''} link={''}/>
-    <Projects image={''} title = {''} description ={''} techstack={''} link={''}/>
+    <div className="projectSection h-screen">
+      <h1 className="text-7xl underline">Projects</h1>
+      <Projects
+        image={
+          "src/assets/circle_small_desk.jpg"
+        }
+        title={"Circle"}
+        description={"An image-based social media app"}
+        techstack={
+          "TypeScript, React, Tailwindcss, shadcn, TanStack query, Appwrite"
+        }
+        link={"https://addtocircle.vercel.app/"}
+      />
     </div>
-  )
+  );
 }
 
 export default ProjectSection
