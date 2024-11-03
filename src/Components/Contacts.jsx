a42b1dc2648b33ef310dfd940ff74090ea3ca0db
Anshu Sarkar <anshusarkaranx@gmail.com>
2024-11-03 16:56:51 +0530

    style changes for contacts section

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -3,7 +3,7 @@ import "./contacts.css"
 const Contacts = () => {
   return (
     <>
-      <hr className="line" />
+      <hr className= " mt-[-3vh] mx-[5vw] border-y-[1.5px]  border-dashed border-y-blackish" />
       <div className="my-10 relative flex justify-evenly">
         <button className=" navButton">
           <a href="mailto:anshusarkaranx@gmail.com" target="_blank">
