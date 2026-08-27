e8ed38f449fb491eede8314655f19ed840671f10
AnshuSarkar <74862392+AnshuSarkarANX@users.noreply.github.com>
2026-08-27 18:19:11 +0530

    feat: simplify emailjs message structure in ContactDialog

--- src/app/_components/ContactDialog.jsx
+++ src/app/_components/ContactDialog.jsx
@@ -41,7 +41,7 @@ export default function ContactDialog({ open, onClose }) {
       await emailjs.send(
         process.env.NEXT_PUBLIC_EJS_SERVICE_KEY ?? "",
         process.env.NEXT_PUBLIC_EJS_TEMPLATE_KEY ?? "",
-        {title:`Message From ${name}`, name: name, email: email, message: `email : ${email}, Message: ${topic}` },
+        {name: name, email: email, message: topic },
       );
       setSavedName(name);
       setSent(true);
