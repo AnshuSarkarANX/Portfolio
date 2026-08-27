d3607efcaedbcfa95f826b106aac83b6d257df06
AnshuSarkar <74862392+AnshuSarkarANX@users.noreply.github.com>
2026-08-27 18:13:56 +0530

    feat: update emailjs integration in ContactDialog and package files

--- src/app/_components/ContactDialog.jsx
+++ src/app/_components/ContactDialog.jsx
@@ -41,7 +41,7 @@ export default function ContactDialog({ open, onClose }) {
       await emailjs.send(
         process.env.NEXT_PUBLIC_EJS_SERVICE_KEY ?? "",
         process.env.NEXT_PUBLIC_EJS_TEMPLATE_KEY ?? "",
-        { from_name: name, from_email: email, topic },
+        {title:`Message From ${name}`, name: name, email: email, message: `email : ${email}, Message: ${topic}` },
       );
       setSavedName(name);
       setSent(true);
